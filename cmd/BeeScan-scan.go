ba3d7203776bd810a8d0f2520867c6c81be646f6
Ameng <amengsec@qq.com>
2022-08-09 16:45:43 +0800

    add ctx in main

--- cmd/BeeScan-scan.go
+++ cmd/BeeScan-scan.go
@@ -15,6 +15,7 @@ import (
 	"BeeScan-scan/pkg/scan/ipinfo"
 	"BeeScan-scan/pkg/util"
 	"BeeScan-scan/pkg/worker"
+	"context"
 	"embed"
 	"fmt"
 	"github.com/fatih/color"
@@ -22,7 +23,9 @@ import (
 	gowap "github.com/jiaocoll/GoWapp/pkg/core"
 	"github.com/olivere/elastic/v7"
 	"github.com/panjf2000/ants/v2"
-	"sync"
+	_ "github.com/projectdiscovery/fdmax/autofdmax"
+	"github.com/remeh/sizedwaitgroup"
+	"go.uber.org/ratelimit"
 	"time"
 )
 
@@ -44,11 +47,12 @@ var (
 	nodestate  *job.NodeState
 	taskstate  *job.TaskState
 	region     *ipinfo.Ip2Region
-	wg         sync.WaitGroup
+	wg         sizedwaitgroup.SizedWaitGroup
 	wapp       *gowap.Wappalyzer
 	esclient   *elastic.Client
 	fofaPrints *fringerprint.FofaPrints
 	GoNmap     *gonmap.VScan
+	rl         ratelimit.Limiter
 	p          *ants.PoolWithFunc
 )
 
@@ -83,94 +87,133 @@ func init() {
 		Finished:  0,
 		LastTime:  time.Time{},
 	}
-	p = worker.WorkerInit(nodestate, taskstate, &wg, conn, GoNmap, region, tmpresults)
+	rl = ratelimit.New(config.GlobalConfig.WorkerConfig.Thread)
+	wg = sizedwaitgroup.New(config.GlobalConfig.WorkerConfig.WorkerNumber)
+	p = worker.WorkerInit(nodestate, taskstate, &wg, rl, conn, GoNmap, region, tmpresults)
 	_, _ = fmt.Fprintln(color.Output, color.HiMagentaString("Initialized!"))
 }
 
 func main() {
-
-	for true {
-		// 扫描节点状态更新
-		node.NodeUpdate(conn, config.GlobalConfig.NodeConfig.NodeName, nodestate)
-
-		// 处理消息队列任务
-		runner.Handlejob(conn, queue, taskstate)
-		var runners []*runner.Runner
-		if queue.Length > 0 { //判断队列长度
-			// 任务实例集合
-			runners = runner.HandleTargets(queue, fofaPrints)
-		}
-
-		if len(runners) > 0 {
-			// 添加任务到任务队列
-			for _, v := range runners {
-				jobs <- v
-				nodestate.Tasks++
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	// 扫描节点状态更新
+	go func() {
+		for true {
+			if nodestate.State == "Running" {
+				log2.InfoOutput("[Tasks]:", nodestate.Tasks, "[Running]:", nodestate.Running, "[Finished]:", nodestate.Finished)
 			}
-			runners = nil
+			node.NodeUpdate(conn, config.GlobalConfig.NodeConfig.NodeName, nodestate)
+			node.TaskUpdate(conn, taskstate)
+			time.Sleep(5 * time.Second)
 		}
+	}()
 
-		if len(jobs) > 0 {
-			nodestate.State = "Running"
-			for i := 0; i < len(jobs); i++ {
-				j := <-jobs
-				wg.Add(1)
-				_ = p.Invoke(j)
+	// 处理消息队列任务
+	go func() {
+		for true {
+			runner.Handlejob(conn, queue, taskstate)
+			var runners []*runner.Runner
+			if queue.Length > 0 { //判断队列长度
+				// 任务实例集合
+				runners = runner.HandleTargets(queue, fofaPrints)
 			}
-			wg.Wait()
-		}
 
-		// Gowapp扫描模块，单独进行扫描，防止rod模块多线程出现panic
-		if len(tmpresults) > 0 {
-			for i := 0; i < len(tmpresults); i++ {
-				tmpres := <-tmpresults
-				if tmpres != nil {
-					if tmpres.Banner != "" {
-						tmpres.Wappalyzer = gowapp.GoWapp(tmpres, wapp)
-						time.Sleep(500 * time.Millisecond)
-						results <- tmpres
+			if len(runners) > 0 {
+				// 添加任务到任务队列
+				for _, v := range runners {
+					jobs <- v
+					nodestate.Tasks++
+				}
+				runners = nil
+			}
+			time.Sleep(5 * time.Second)
+		}
+	}()
+
+	go func() {
+		for true {
+			if len(jobs) > 0 {
+				nodestate.State = "Running"
+				for i := 0; i < len(jobs); i++ {
+					j := <-jobs
+					wg.Add()
+					_ = p.Invoke(j)
+				}
+				wg.Wait()
+			}
+			time.Sleep(5 * time.Second)
+		}
+	}()
+
+	// Gowapp扫描模块，单独进行扫描，防止rod模块多线程出现panic
+	go func() {
+		for true {
+			if len(tmpresults) > 0 {
+				for i := 0; i < len(tmpresults); i++ {
+					tmpres := <-tmpresults
+					if tmpres != nil {
+						if tmpres.Banner != "" || tmpres.Servername == "http" {
+							tmpres.Wappalyzer = gowapp.GoWapp(tmpres, wapp)
+							time.Sleep(500 * time.Millisecond)
+							results <- tmpres
+						}
+					}
+				}
+			}
+			time.Sleep(5 * time.Second)
+		}
+	}()
+
+	go func() {
+		for true {
+			if len(results) > 0 {
+				// 遍历结果，写入数据库
+				for i := 0; i < len(results); i++ {
+					res := <-results
+					if res != nil {
+						if res.Target != "" {
+							db.EsAdd(esclient, res)
+						}
 					}
 				}
 			}
 		}
+	}()
 
-		if len(results) > 0 {
-			// 遍历结果，写入数据库
-			for i := 0; i < len(results); i++ {
-				res := <-results
-				if res != nil {
-					if res.Target != "" {
-						db.EsAdd(esclient, res)
+	// 写入日志到es数据库
+	go func() {
+		for true {
+			if util.MinSub(db.QueryLogByID(esclient, config.GlobalConfig.NodeConfig.NodeName))%30 == 0 && util.MinSub(db.QueryLogByID(esclient, config.GlobalConfig.NodeConfig.NodeName)) > 0 {
+				db.ESLogAdd(esclient, "BeeScanLogs.log")
+			}
+			time.Sleep(30 * time.Second)
+		}
+	}()
+
+	// 扫描节点状态更新
+	//node.NodeUpdate(conn, config.GlobalConfig.NodeConfig.NodeName, nodestate)
+	go func() {
+		for true {
+			if len(jobs) == 0 {
+				log2.InfoOutput("[ConnectCheck]")
+				nodestate.State = "Free"
+				taskstate.Name = ""
+				taskstate.Running = 0
+				taskstate.Finished = 0
+				// 节点每运行15天会定时重新扫描es数据库中一定时长的目标
+				if util.DaySub(nodestate.StartTime) > 15 && util.DaySub(nodestate.StartTime)%15 == 0 {
+					RegularTargets := db.EsScanRegular(esclient)
+					RegularTargets = util.Removesamesip(RegularTargets)
+					if RegularTargets != nil {
+						log2.Info("[targets]:", RegularTargets)
+						for _, v := range RegularTargets {
+							job.Push(queue, v)
+						}
 					}
 				}
+				time.Sleep(10 * time.Second)
 			}
 		}
-
-		// 写入日志到es数据库
-		if nodestate.State == "Free" && util.MinSub(db.QueryLogByID(esclient, config.GlobalConfig.NodeConfig.NodeName))%30 == 0 && util.MinSub(db.QueryLogByID(esclient, config.GlobalConfig.NodeConfig.NodeName)) > 0 {
-			db.ESLogAdd(esclient, "BeeScanLogs.log")
-		}
-
-		// 扫描节点状态更新
-		node.NodeUpdate(conn, config.GlobalConfig.NodeConfig.NodeName, nodestate)
-		if len(jobs) == 0 {
-			log2.InfoOutput("[ConnectCheck]")
-			nodestate.State = "Free"
-			taskstate.Name = ""
-			taskstate.Running = 0
-			taskstate.Finished = 0
-			// 节点每运行15天会定时重新扫描es数据库中一定时长的目标
-			if util.DaySub(nodestate.StartTime) > 15 && util.DaySub(nodestate.StartTime)%15 == 0 {
-				RegularTargets := db.EsScanRegular(esclient)
-				RegularTargets = util.Removesamesip(RegularTargets)
-				if RegularTargets != nil {
-					log2.Info("[targets]:", RegularTargets)
-					for _, v := range RegularTargets {
-						job.Push(queue, v)
-					}
-				}
-			}
-			time.Sleep(10 * time.Second)
-		}
-	}
+	}()
+	<-ctx.Done()
 }
